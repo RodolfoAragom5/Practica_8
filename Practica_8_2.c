7282810308f7128cab18944a1483118cf3507522
RodolfoAragom5 <143585799+RodolfoAragom5@users.noreply.github.com>
2024-10-04 13:52:51 -0600

    Update Practica_8_2.c

--- Practica_8_2.c
+++ Practica_8_2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+main ()
+{
+  int n, i=1, r;
+
+  printf("Favor de ingresar un numero del que desea saber la tabla de multiplicar\n");
+  scanf("%d", &n);
+  
+  while( i<=10)
+    {
+      r=n*i;
+      printf("%d*%d=%d\n", n, i, r);
+      i=i+1;
+    }
+  return 0;
+}
