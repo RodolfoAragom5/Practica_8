67f2e3a94c28b00fd8fa2303e30ee82fb0f3f1cb
RodolfoAragom5 <143585799+RodolfoAragom5@users.noreply.github.com>
2024-10-04 13:39:27 -0600

    Update Practica_8_1.c

--- Practica_8_1.c
+++ Practica_8_1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+main ()
+{
+  int n, i, r;
+
+  printf("Favor de ingresar un numero del que desea saber la tabla de multiplicar\n");
+  scanf("%d", n);
+
+  for( i=1; i<10; i++)
+    {
+      printf("%d", &r);
+    }
+  return 0;
+}
